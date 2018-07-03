a4fe66e02496ecc1e83ae0c225b8cb2cdd3fffbd
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-03 14:30:11 -0700

    HightLowGame - RobotColorChooser

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,20 +5,27 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(101);
 		
 		// 2. Print out the random variable above
+		System.out.println(random);
+		boolean Continue = true;
+		
 		
 		// 11. Repeat steps 1 to 10 ten times
-		
+			while (Continue == true){
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
+				int Response = Integer.parseInt(JOptionPane.showInputDialog("Guess a number between 0-100."));
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
+				if (Response == random) {
+					JOptionPane.showMessageDialog(null, "Correct!");
+					System.exit(0);
 			// 5. if the guess is correct
 				// 6. Win
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
@@ -26,9 +33,15 @@ public class HighLowGame {
 				// 8. Tell them it's too high
 			// 9. if the guess is low
 				// 10. Tell them it's too low
-
+				}else {
+					if (Response>random) {
+						JOptionPane.showMessageDialog(null, "Too high! Try again.");
+					}else {
+						JOptionPane.showMessageDialog(null, "Too low! Try again.");
+					}
+				}
 		// 13. Tell them they lose
-		
+			}
 	}
 
 }
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -5,23 +5,53 @@ package elseif;
 
 import javax.swing.JOptionPane;
 import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
+import java.util.Random;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+		Random r = new Random();
 		//1. Create a new Robot
-		
+		Robot robot = new Robot("mini"); 
 		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
-
-        //6. If the user doesn’t enter anything, choose a random color
-
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
-
+		robot.penDown();
+		robot.setPenWidth(10);
+		robot.setSpeed(10000000);
+		for (int i=1;i<i+1;i++) {
+			String UserColor = JOptionPane.showInputDialog("Pick a color");
+			//5. Use an if/else statement to set the pen color that the user requested
+			if (UserColor.equalsIgnoreCase("orange")) {
+				robot.setPenColor(Color.orange);
+			}else if (UserColor.equalsIgnoreCase("cyan")){
+				robot.setPenColor(Color.cyan);
+			}else if (UserColor.equalsIgnoreCase("gray")){
+				robot.setPenColor(Color.gray);
+			}else if (UserColor.equalsIgnoreCase("green")){
+				robot.setPenColor(Color.green);
+			}else if (UserColor.equalsIgnoreCase("red")){
+				robot.setPenColor(Color.red);
+			}else if (UserColor.equalsIgnoreCase("yellow")){
+				robot.setPenColor(Color.yellow);
+			}else if (UserColor.equalsIgnoreCase("pink")){
+				robot.setPenColor(Color.pink);
+			}else if (UserColor.equalsIgnoreCase("magenta")){
+				robot.setPenColor(Color.magenta);
+			}else if (UserColor.equalsIgnoreCase("blue")){
+				JOptionPane.showMessageDialog(null, "[Color Not Found. Default Color: Black] ");
+				robot.setRandomPenColor();
+			}
+	        //6. If the user doesn’t enter anything, choose a random color
+			int RandomSides = r.nextInt(7)+2;
+			int DegreesToTurn = 360/RandomSides;
+	        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+			for (int w=1;w<=RandomSides;w++) {
+				robot.move(50);
+				robot.turn(DegreesToTurn);
+			}
+			//4. Set the pen width to 10
+			
+		    //2. Make the robot draw a shape (this will take more than one line of code)
+		}
 
 	}
 }
